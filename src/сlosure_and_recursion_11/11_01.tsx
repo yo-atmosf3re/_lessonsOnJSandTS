b7daca17c4904c602f5350e9332d220c690f2343
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-08-30 21:25:36 +0300

    completed the study of recursion, stack, stack overflow and scope; sorted out some tasks by recursions;

--- src/сlosure_and_recursion_11/11_01.tsx
+++ src/сlosure_and_recursion_11/11_01.tsx
@@ -3,6 +3,11 @@ import * as React from 'react';
 // Лексическое окружение, замыкание и рекурсия
 // Скоуп - правило поиска переменной в определенной области видимости
 // Лексическое окружение - то, что окружает функцию/блок кода и то, на что ссылается функция/блок кода при поиске переменных
+// let и const видны в пределах блока и видимости, а var не ограничивается пределами блока
+// Стэк - структура данных, в которой пооредно хранятся данные. В стэк попадает первый вызов функции, последним он и уходит
+// Рекурсия вызывается до тех пор, пока не удалятся все стэк-фреймы из стека, и функция не вернёт примитивное значение
+// Шаг рекурсии - вызов рекурсии ещё раз
+// Если не будет условия, после которого рекурсия прекратится, то будет переполнение стэка (stack oferflow)
 
 function Foo() {
    // let firstName = 'Alex'
@@ -31,19 +36,65 @@ function Foo() {
    // }
    // sayHello('Alex')
 
-   function makeCounter() {
-      let count = 0
-      return () => {
-         console.log(++count)
-      }
-   }
-   let counter = makeCounter();
-   let counter2 = makeCounter();
-   counter()
-   counter()
-   counter2()
-   counter2()
 
+   // function makeCounter() {
+   //    let count = 0
+   //    return () => {
+   //       console.log(++count)
+   //    }
+   // }
+   // let counter = makeCounter();
+   // let counter2 = makeCounter();
+   // counter()
+   // counter()
+   // counter2()
+   // counter2()
+
+   // const a = () => {
+   //    const aName = 'a'
+   //    console.log(aName)
+   // }
+   // const b = () => {
+   //    const bName = 'b'
+   //    console.log(bName)
+   //    a()
+   // }
+   // const c = () => {
+   //    const cName = 'c'
+   //    console.log(cName)
+   //    b()
+   // }
+   // c()
+
+   // const pow = (x: number, n: number): number => {
+   //    if (n === 1) {
+   //       return x
+   //    }
+   //    return x * pow(x, n - 1)
+   // }
+   // console.log(pow(2, 5))
+
+   const dom = {
+      nodeValue: 5,
+      children: [
+         { nodeValue: 5, children: null },
+         {
+            nodeValue: 7, children: [
+               { nodeValue: 4, children: null }
+            ]
+         }
+      ]
+   }
+
+   const domRecursion = (dom: any) => {
+      let res = dom.nodeValue;
+      if (!dom.children) {
+         return res
+      }
+      dom.children.forEach((n: any) => {
+         res += domRecursion(n)
+      })
+   }
 
 
 
