d44d81b6ceb6e6865567a116f78457be1ffdda6c
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-09-10 12:06:43 +0300

    I continue to study promises again;

--- src/promiseAgain_14/promiseAgain_14.tsx
+++ src/promiseAgain_14/promiseAgain_14.tsx
@@ -2,6 +2,16 @@ import axios from 'axios';
 import React from 'react'
 
 function PromiseAgainExample() {
+   function getNumber() {
+      const promise = new Promise((resolve, reject) => {
+         setTimeout(() => {
+            resolve(Math.random())
+         }, 2000);
+      })
+      return promise;
+   }
+   getNumber().then(n => console.log(n))
+   getNumber().then(n => console.log(n))
    return (<div>
       Promise example, again...
    </div>);
