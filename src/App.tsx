62138c9570ac9fc1d62a0b0b404db0cb6caea275
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-09-10 10:22:13 +0300

    I study promises again to better consolidate the educational material;

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import EventLoopExample from './eventLoop_13/eventLoop_13';
+import PromiseAgainExample from './promiseAgain_14/promiseAgain_14';
 import PromiseExample from './promise_12/12';
 
 
 function App() {
   return (
-    <div className="App">
-      <EventLoopExample />
+    <div style={{ textAlign: 'center', fontSize: '40px' }}>
+      <PromiseAgainExample />
     </div>
   );
 }
---
+++ src/promiseAgain_14/promiseAgain_14.tsx
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import React from 'react'
+
+function PromiseAgainExample() {
+   return (<div>
+      Promise example, again...
+   </div>);
+}
+
+export default PromiseAgainExample;
